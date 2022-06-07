f25ec655dbe7390b1cd51824f8a82e9bf2b53a5b
Jorge <jorch.rl@gmail.com>
2022-06-07 12:36:50 -0500

    Add first working version of the service
    - Added dependencies (Multer, Sharp)
    - Setup Multer with memorystorage. Then a buffer is passed to lechonkify() and into sharp.
    - Sharp's output (.toBuffer()) is sent as a response from express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const express = require("express")
+const multer = require("multer")
+const morgan = require("morgan")
+
 const lechonkify = require("./lechonkifier.js").lechonkify
+
 const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use(morgan("combined"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", express.static("test-client"))
 
-app.post("/lechonkifier", lechonkify)
+/////////
+//I want to receive files to memory. As a in-memory buffer
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+app.post("/lechonkifier", upload.single("pic"), lechonkify)
+
+///////////
 
 app.listen(PORT, () => {
     console.log(`App started on port ${PORT}`)
--- src/lechonkifier.js
+++ src/lechonkifier.js
@@ -1,7 +1,28 @@
+const sharp = require("sharp")
 
 
-const lechonkify = (req, res) => {
+const lechonkify = async (req, res, next) => {
+
     console.log("lechonkifying :3")
+
+    // req should have these from multer.
+    console.log("body", req.body)
+    console.log("file", req.file)
+
+
+
+    const inputImage = req.file.buffer
+    try {
+        const imageBuffer = await sharp(inputImage)
+            .grayscale()
+            .png()
+            .toBuffer()
+
+        res.send(imageBuffer)
+    } catch (e) {
+        next(e)
+    }
+    // res.status(200).end()
 }
 
 module.exports = { lechonkify }
